397ff124fd32cf4e63c49179a46805e7885dbbb7
Aneeshkhandve <156901195+Aneeshkhandve@users.noreply.github.com>
2024-07-31 18:35:51 +0530

    Delete task_manager/starter directory

--- task_manager/starter/db/connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const connectionString = 'mongodb+srv://khandveaneesh:aKwUDXNyoODs0n7k@nodeexpressprojects.2t6mqoe.mongodb.net/'
-
-mongoose.connect(connectionString)
-.then(()=>console.log("Connected to db..."))
-.catch((err) => console.log(err));
